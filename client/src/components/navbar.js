1cbc0d42262c10d67dd8ad1156abf5b55c88e2c2
Lamor Odingo <l2odingo@gmail.com>
2022-07-09 20:04:27 -0700

    styled nav buttons

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,17 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 export default function Navbar() {
   return (
     <div>
       <div className="navbar">
-        <h1>ThatsMyJam!</h1>
+        <h1>
+          <i>T h a t s M y J a m !</i>
+        </h1>
         <div>
           <ul className="nav-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/library">Library</Link></li>
-            <li><Link to="Saved">Saved</Link></li>
-            <li><Link to="Signout">Signout</Link></li>
+            <div className="button-border">
+              <li className= "list-text">
+                <Link to="/" style={{textDecoration: "none", color: "white"}}>Home</Link>
+              </li>
+            </div>
+            <div className="button-border">
+              <li>
+                <Link to="/library" style={{textDecoration: "none", color: "white"}}>Library</Link>
+              </li>
+            </div>
+            <div className="button-border">
+              <li>
+                <Link to="Saved" style={{textDecoration: "none", color: "white"}}>Saved</Link>
+              </li>
+            </div>
+            <div className="button-border">
+              <li>
+                <Link to="Signout" style={{textDecoration: "none", color: "white"}}>Signout</Link>
+              </li>
+            </div>
           </ul>
         </div>
       </div>
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -4,7 +4,11 @@ import React from 'react';
 export default function Signup() {
     return (
         <div>
-            
+            <div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
